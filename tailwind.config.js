8f2caf4677d74141896766159995403fb6414b4d
JiangShuuu <globelex65@gmail.com>
2022-12-15 10:48:15 +0800

    first

---
+++ tailwind.config.js
@@ -0,0 +1,54 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        red: {
+          DEFAULT: '#ff3b3f',
+          primary: '#DD4B39'
+        },
+        orange: {
+          DEFAULT: '#ff861d',
+          primary: '#FF9933'
+        },
+        yellow: {
+          DEFAULT: '#ffae34',
+          primary: '#e9dfd1'
+        },
+        blue: {
+          DEFAULT: '#76bad2',
+          primary: '#3D5998'
+        },
+        black: {
+          DEFAULT: '#000000',
+          primary: '#1e170f',
+          shallower: '#444444',
+        },
+        white: {
+          DEFAULT: '#ffffff',
+          primary: '#efefef'
+        },
+        gray: {
+          DEFAULT: '#b8b8b8',
+          primary: '#979797',
+          depper: '#6f6f6f',
+          dark: '#555057'
+        },
+        brown: {
+          DEFAULT: '#423334'
+        }
+      },
+    },
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+    },
+  },
+  plugins: [],
+}
