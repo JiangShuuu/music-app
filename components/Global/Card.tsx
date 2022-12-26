5fb6ec0ea377910c42d357a620e2969131dcfa64
JiangShuuu <globelex65@gmail.com>
2022-12-26 21:12:59 +0800

    upd

--- components/Global/Card.tsx
+++ components/Global/Card.tsx
@@ -7,21 +7,24 @@ import { CardActionArea } from '@mui/material'
 
 export default function Cards() {
   return (
-    <Card sx={{ maxWidth: 345 }}>
+    <Card sx={{ maxWidth: 200, maxHeight: 300 }}>
       <CardActionArea>
         <CardMedia
           component="img"
-          height="140"
           image="/images/01.png"
+          className="w-48"
           alt="green iguana"
         />
         <CardContent>
           <Typography gutterBottom variant="h5" component="div">
-            Lizard
+            週末清聽
           </Typography>
-          <Typography variant="body2" color="text.secondary">
-            Lizards are a widespread group of squamate reptiles, with over 6,000
-            species, ranging across all continents except Antarctica
+          <Typography
+            variant="body2"
+            color="text.secondary"
+            className="line-clamp line-clamp-2"
+          >
+            到了週末,你應該試著把柔軟的感覺找回來,小小小到了週末,你應該試著把柔軟的感覺找回來,小小小到了週末,你應該試著把柔軟的感覺找回來,小小小
           </Typography>
         </CardContent>
       </CardActionArea>
